8caed218fdc35257b9b544f04422c96c41c3fb6e
wd5gnr <al.williams@awce.com>
2020-07-07 00:13:05 -0500

    Made format include volume label and better option handling

--- format.c
+++ format.c
@@ -1,15 +1,16 @@
 // Al Williams Hackaday
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Specific for MPC Z80 -- does not use BIOS/BDOS calls but talks directly to IOS
 
 
 int main(int argc, char *argv[])
 {
-  if (argc!=2&&argc!=3)
+  if (argc<2 || argc>4)
     {
-      printf("Usage: FORMAT DRIVE [S] where DRIVE is 0-3\nUse S to do a system generation using the current\ndrive 0 system tracks (default for drive 0)");
+      printf("Usage: FORMAT DRIVE [LABEL] [/S] where DRIVE is 0-3\nUse /S to do a system generation using the current\ndrive 0 system tracks (default for drive 0)");
       return 1;
     }
   unsigned sysgen=0;
@@ -18,22 +19,34 @@ int main(int argc, char *argv[])
   unsigned disknr=atoi(argv[1]);
   unsigned int track, c, sector, i, j;
   unsigned int percent=0;
+  char label[13];
+  int argind=2;
   if (disknr<0||disknr>3)
     {
       printf("Unknown disk drive\n");
       return 1;
     }
-  if (argc==3)
+  while (argc>argind)
     {
-      if (*argv[2]=='S' || *argv[2]=='s')
-	sysgen=1;
-      else
+      if (argv[argind][0]=='/')
 	{
-	  printf("Unknown option\n");
+	if ((argv[argind][1]=='s'|| argv[argind][1]=='S'))
+	  {
+	  sysgen=1;
+	  argind++;
+	  continue;
+	  }
+        else
+	  {
+	  printf("Unknown option: argv[argind]\n");
 	  return 1;
+	  }
 	}
+      strcpy(label+1,argv[argind]);  // label name
+      argind++;
     }
-
+      
+  label[0]=(char)0x20;  // label marker for directory
   if (disknr==0) sysgen=1;
   if (sysgen)
     {
@@ -50,7 +63,10 @@ int main(int argc, char *argv[])
 	    }
 	}
     }
-  printf("This will format drive %d (%c:) and destroy all contents on the drive. Are you sure? ",disknr,'A'+disknr);
+  printf("This will format drive %d (%c:) as volume %s and destroy all contents on the drive. Are you sure? ",disknr,'A'+disknr,label+1);
+  for (i=strlen(label);i<sizeof(label)-1;i++) label[i]=' '; // blank pad
+  label[sizeof(label)-1]=(char)1;  // not '1' but 1  -- label marker
+
   do {
     c=getchar();
     if (c=='n' || c=='N'||c==27||c==3) return 1;
@@ -68,7 +84,13 @@ int main(int argc, char *argv[])
 	  outp(11,sector);
 	  for (i=0;i<128;i++)
 		{
-		  if (sysgen && track<2) byte=sys[(track*32+sector-1)*128+i];
+		  byte=0xE5;
+		  if ((sysgen && track==2) || (sysgen==0 && track==0))
+		    {
+		      if (sector==1 && i<sizeof(label)) byte=label[i];
+		    }
+   	          else
+		    if (sysgen && track<2) byte=sys[(track*32+sector-1)*128+i];
 		  outp(12,byte);
 		} // end outp loop
 	}  // end sector
